a2fd5dc27cd50aee5195e75ff4537eddad0478af
Ariful2016 <arifewucse2016@gmail.com>
2022-03-13 15:39:58 -0500

    first c

---
+++ app/src/main/java/com/arifcit/testmemory/utils/Constants.kt
@@ -0,0 +1,35 @@
+package com.arifcit.testmemory.utils
+
+import com.arifcit.testmemory.R
+
+const val EXTRA_GAME_NAME = "EXTRA_GAME_NAME"
+const val EXTRA_BOARD_SIZE = "EXTRA_BOARD_SIZE"
+val DEFAULT_ICONS = listOf(
+  R.drawable.apple,
+  R.drawable.car,
+  R.drawable.cat,
+  R.drawable.panda,
+  R.drawable.pepe,
+  R.drawable.love,
+  R.drawable.bike,
+  R.drawable.bird,
+  R.drawable.dog,
+  R.drawable.watermalon,
+  R.drawable.mango,
+  R.drawable.football
+)
+/*
+val DEFAULT_ICONS = listOf(
+  R.drawable.ic_face,
+  R.drawable.ic_flower,
+  R.drawable.ic_gift,
+  R.drawable.ic_heart,
+  R.drawable.ic_home,
+  R.drawable.ic_lightning,
+  R.drawable.ic_moon,
+  R.drawable.ic_plane,
+  R.drawable.ic_school,
+  R.drawable.ic_send,
+  R.drawable.ic_star,
+  R.drawable.ic_work
+)*/
